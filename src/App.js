a4abc52e6eeb18f1f697c21ff85fca4eac3ba6ee
jackdudley <jack.dudley340@gmail.com>
2025-06-23 19:25:05 -0400

    changing tracking of board to be based on history instead of copy of array found in the board component. This required making another component on top of Board called Game in order to bring the board the most accurate last move based on the history so that all of it is maintained throughout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import { useState } from 'react';
 
-export default function Board() {
-    const [squares, setSquares] = useState(Array(9).fill(null));
+
+export default function Game() {
     const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        // TODO
+      }
+    
+
+    return (
+        <div className="game">
+        <div className="game-board">
+            <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+            <ol>{/*TODO*/}</ol>
+        </div>
+        </div>
+    );
+}
+
+
+
+function Board({xIsNext, squares, onPlay}) {
+  //  const [squares, setSquares] = useState(Array(9).fill(null));
+   // const [xIsNext, setXIsNext] = useState(true);
     function handleClick(i) {
         const nextSquares = squares.slice();
-        if(squares[i]) {
+        if(squares[i] || calculateWinner(squares)) {
             return;
         }
         if(xIsNext) {
@@ -15,9 +40,20 @@ export default function Board() {
         }
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
+
+    let status;
+        const winner = calculateWinner(squares);
+        if(winner != null) {
+            status = "Winner: " + winner;
+        } else {
+           status = "Next player: " + (xIsNext ? "X" : "O");
+        }
+
     return (
     <>
+       <div className="status">{status}</div>
        <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -42,4 +78,24 @@ function Square({value, onSquareClick}) {
         <button className="square" onClick={onSquareClick}>{value}</button>
     );
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
